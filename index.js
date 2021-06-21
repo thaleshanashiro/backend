1eee826113b936e4c807293d69dd989c991e2228
thaleshanashiro <73593558+thaleshanashiro@users.noreply.github.com>
2021-06-21 10:50:50 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,114 +1,172 @@
-const express = require ("express");
+const express = require("express");
 const app = express();
 app.use(express.json());
 
-//teste
-//permissoes
-var cors = require ('cors');
+// Permissões
+var cors = require('cors');
 app.use(cors());
 
-//porta que estou ouvindo
+// Porta que eu estou ouvindo
 app.listen(process.env.PORT || 3000);
 
-app.get('/',function (req, res) {
-res.send("<h2>Times do campeonato brasileiro 2021, serie A:</h2>");
-} 
-  );
+app.get('/', 
+    function (req, res){    
+        res.send("Hello World");
+    }
+);
 
- //Array de um object com os nomes dos times e o Estado de cada um 
-const times = [ 
-   {time: "Corinthians "}, 
-   {time: "Santos "}, 
-   {time: "Sao Paulo "},
-   {time: "Flamengo "},
-   {time: "America-MG "},
-   {time: "Atletico-PR "},
-   {time: "Atletico-GO"},
-   {time: "Bahia"},
-   {time: "Ceara SC "},
-   {time: "Chapecoense"},
-   {time: "Cuiaba "},
-   {time: "Fluminense"},
-   {time: "Fortaleza"},
-   {time: "Gremio"},
-   {time: "Internacional"},
-   {time: "Juventude"},
-   {time: "Palmeiras "},
-   {time: "Bragantino"},
-   {time: "Sporte Recife"},
-   {time: "Atletico MG"}
-   
-  ];
+app.get('/hello',
+function (req, res){    
+    res.send("Hello de Novo");
+    }
+)
 
-//imprime o banco de dados
-app.get('/times',function(req, res)  {
- res.send(times.filter(Boolean)); 
-  }
-    );
+const mensagens = [
+    "Elziele da Rocha", "Lucas Canova dos Santos", 0 
+];
 
-//busca no banco de dados usando o id, ou seja, busca cada linha da matriz em especifico
-  app.get('/times/:id', function(req, res){
-     const id = req.params.id - 1;
-     const time = times [id];
-     if (!time){
-       res.send("time nao encontrada");
-            }
-            else {
-              res.send(time);
-            }
-  }
-  );
+app.get('/mensagens',
+    function(req, res){
+        // res.send(mensagens);
+        res.send(mensagens.filter(Boolean));
+    }
+);
 
-  //adciona um time na matriz 
-  app.post('/times', (req,res) =>{
-       console.log(req.body.time);
-       const time = req.body.time;
-       times.push(time);
-       res.send("Adcionar um time")
-  }
-  );
+app.get('/mensagens/:id',
+    function(req, res){
+        const id = req.params.id - 1;
+        const time = times[id];
 
-  //troca um time da matriz por outro
-  app.put('/times/:id', 
-     (req, res) => {
-       const id = req.params.id -1;
-       const time = req.body.time;
-       times[id] = time;
-       res.send("time atulizado com sucesso")
-     }
-  )
-  
-  //deleta um time da matriz
-  app.delete('/times/:id',
-  (req, res) => {
-     const id = req.params.id -1;
-     delete times[id];
-     res.send("time removida com sucesso");
-  }
-  );
+        if (!time){
+            res.send("Time não encontrado");
+        } else {
+            res.send(time);
+        }
+    }
+)
 
+app.post('/mensagens', 
+    (req, res) => {
+        console.log(req.body.time);
+        const time = req.body.time;
+        mensagens.push(time);
+        res.send("Adicionar um time.")
+    }
+);
+
+app.put('/mensagens/:id',
+    (req, res) => {
+        const id = req.params.id - 1;
+        const time = req.body.time;
+        mensagens[id] = time;        
+        res.send("Time atualizado com sucesso.")
+    }
+)
+
+app.delete('/mensagens/:id', 
+    (req, res) => {
+        const id = req.params.id - 1;
+        delete times[id];
+
+        res.send("Time removido com sucesso");
+    }
+);
+
+/*
+  Daqui para baixo, uso o banco de dados MongoDB
+*/
 
 const mongodb = require('mongodb')
-const password = process.env.password || "asdf";
+const password = process.env.PASSWORD || "asdf";
 console.log(password);
 
-const connectionString = 'mongodb+srv://admin:${password}@cluster0.tomq4.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
-const options = {
-  useNewUrlParser: true, 
-  useUnifiedTopology: true
+const connectionString = `mongodb+srv://admin:${password}@cluster0.tomq4.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+const options = { 
+    useNewUrlParser: true, 
+    useUnifiedTopology: true 
 };
 
 (async()=>{
-  const client = await mongodb.MongoClient.connect{connectionString, options};
-  const db = client.db{'myFirstDatabase'};
-  const times = db.collection('times');
-  console.log(awai times.find({}).toArray());
+    const client = await mongodb.MongoClient.connect(connectionString, options);
+    const db = client.db('myFirstDatabase');
+    const times = db.collection('times');
+    console.log(await times.find({}).toArray());
+
+    app.get('/database',
+        async function(req, res){
+        res.send(await times.find({}).toArray());
+    }
+);
+
+app.get('/database/:id',
+    async function(req, res){
+        const id = req.params.id;
+        const time = await times.findOne(
+            {_id : mongodb.ObjectID(id)}
+        );
+        console.log(time);
+        if (!time){
+            res.send("Time não encontrado");
+        } else {
+            res.send(time);
+        }
+    }
+);
+
+app.post('/database', 
+    async (req, res) => {
+        console.log(req.body);
+        const time = req.body;
+        
+        delete time["_id"];
+
+        mensagens.insertOne(time);        
+        res.send("Adicionar um time.");
+    }
+);
+
+app.put('/database/:id',
+    async (req, res) => {
+        const id = req.params.id;
+        const time = req.body;
+
+        console.log(time);
+
+        delete time["_id"];
+
+        const num_mtimes = await times.countDocuments({_id : mongodb.ObjectID(id)});
+
+        if (num_times !== 1) {
+            res.send('Ocorreu um erro por conta do número de times');
+            return;
+        }
+
+        await mensagens.updateOne(
+            {_id : mongodb.ObjectID(id)},
+            {$set : time}
+        );
+        
+        res.send("Time atualizado com sucesso.")
+    }
+)
+
+app.delete('/database/:id', 
+    async (req, res) => {
+        const id = req.params.id;
+        
+        await times.deleteOne({_id : mongodb.ObjectID(id)});
+
+        res.send("Time removido com sucesso");
+    }
+);
+
 })();
 
 
+/*
 
-  /*
-  const MongoClient = require('mongodb').MongoClient;
+const MongoClient = require('mongodb').MongoClient;
 const uri = "mongodb+srv://admin:<password>@cluster0.tomq4.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
@@ -116,6 +174,3 @@ client.connect(err => {
   // perform actions on the collection object
   client.close();
 });
-
-
-senha: dkbDSLqvWq4vy2F5
